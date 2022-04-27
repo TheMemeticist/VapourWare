ac04e57f74d7b27b0a68200361027f986e1f5dcd
Epi-Yeti <77855700+TheMemeticist@users.noreply.github.com>
2022-04-26 23:37:17 -0600

    Add files via upload

---
+++ precache-manifest.32815900e5c2483d5b57805e04a02909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a612b65fcb6b655c0e5",
+    "url": "/VapourWare/static/css/main.de52ded9.chunk.css"
+  },
+  {
+    "revision": "0a612b65fcb6b655c0e5",
+    "url": "/VapourWare/static/js/main.0a612b65.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/VapourWare/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "3fa05e88fab27d205962",
+    "url": "/VapourWare/static/js/2.3fa05e88.chunk.js"
+  },
+  {
+    "revision": "264529ed6109b756b570",
+    "url": "/VapourWare/static/js/runtime~main.264529ed.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/VapourWare/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ac4f9bce5b1e4c9f147943288d92867e",
+    "url": "/VapourWare/index.html"
+  }
+];
